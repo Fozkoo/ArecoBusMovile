275433f68e0deb3c30f39b27c8540bca31567409
tiziano <tmartinelli@alumnos.unsada.edu.ar>
2024-09-16 21:45:45 -0300

    4.0

--- src/pages/PuntosSube.tsx
+++ src/pages/PuntosSube.tsx
@@ -10,37 +10,37 @@ function PuntosSube() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); 
+    }, 1000);
 
-    
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <IonApp>
-      <IonContent>
+      <IonContent className="fullscreen">
         {loading ? (
-          <Loader /> 
+          <Loader />
         ) : (
           <div>
             <div className="container-header fixed top-0 left-0 w-full bg-white z-50 shadow-sm">
-              <Header/>
+              <Header />
             </div>
 
-            <div className="container-page   pt-[65px] flex items-center mt-7 p-7 flex-col h-[100%]">
-              <div className="container-title-punto-sube  flex justify-center items-center text-center flex-col w-full">
+            <div className="container-page  pt-[65px] flex items-center  mt-7 p-7 flex-col min-h-[100vh]">
+              <div className="container-title-punto-sube flex justify-center items-center text-center flex-col w-full">
                 <h1 className="text-2xl font-bold text-black">Puntos SUBE </h1>
-                <p className="text-base text-black font-semibold">Encontrá tu punto SUBE más cercano </p>
+                <p className="text-base text-black font-semibold">
+                  Encontrá tu punto SUBE más cercano
+                </p>
               </div>
 
-              <div className="container-map   flex rounded-xl shadow-2xl overflow-hidden items-center justify-center w-[90%] h-[500px] mt-5">
-                <MapView/>
+              <div className="container-map mt-7  flex w-[50%] h-[450px] rounded-xl shadow-2xl overflow-hidden items-center justify-center max-lg:w-[90%] max-lg:h-[500px]">
+                <MapView />
               </div>
 
-              <div className="other-text  flex justify-center text-center w-full mt-7 font-semibold">
+              <div className="other-text flex justify-center text-center w-full mt-7 font-semibold">
                 <p>Recorda que tambien podes recargar tu tarjeta SUBE desde Mercado Pago!</p>
               </div>
             </div>
