d359428fcdf1edeeda7e6b980c68485d34828b43
Tiziano <tmartinelli@alumnos.unsada.edu.ar>
2025-01-22 10:25:45 -0300

    feat: banner fix bug desing

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,13 +5,12 @@ interface BannerProps {
     empresaNombre: string;
     origen: string
     destino: string
-    isActive: boolean
 }
 
-const Banner: React.FC<BannerProps> = ({ image, empresaNombre, origen, destino, isActive }) => {
+const Banner: React.FC<BannerProps> = ({ image, empresaNombre, origen, destino }) => {
     return (
         <>
-            <div className="container-card-principal flex flex-col mx-8 mt-5   rounded-lg shadow-xl overflow-hidden">
+            <div className="container-card-principal flex flex-col mx-8 mt-5 rounded-lg shadow-xl overflow-hidden lg:max-w-4xl lg:mx-auto">
                 <div className="h-[110px] relative">
                     <img src={image} className="absolute inset-0 w-full h-full object-cover " alt="Background" />
                     <div className="absolute inset-0 bg-gray-700 bg-opacity-70 "></div>
@@ -28,7 +27,6 @@ const Banner: React.FC<BannerProps> = ({ image, empresaNombre, origen, destino, 
                     <p className="text-gray-600 font-normal text-base">{origen} / {destino}</p>
                 </div>
             </div>
-
         </>
     )
 }
